6e20638c52216568917166e4c7db40c7473cbd4b
Winnie Fong <winniefong21@gmail.com>
2019-04-15 11:54:53 -0700

    Implement RetiredForagerBee to be subclass of ForagerBee

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,32 @@
-var RetiredForagerBee = function() {
+var RetiredForagerBee = function () {
+  ForagerBee.call(this, RetiredForagerBee);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
 
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+RetiredForagerBee.prototype.forage = function () {
+  return 'I am too old, let me play cards instead';
+};
+
+RetiredForagerBee.prototype.gamble = function (treasure) {
+  this.treasureChest.push(treasure);
+};
+
+// Create a RetiredForagerBee class, in pseudoclassical style, with:
+// call the ForagerBee superclass
+// set the prototype
+// set the constructor
+// an age property that is set to 40
+// a job property that is set to gamble
+// a canFly property that is set to false
+// a color property that is set to grey
+// a forage method that returns "I am too old, let me play cards instead"
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a treasureChest property inherited from ForagerBee that is set to an empty array []
+// an always winning gamble method that allows the bee to add a treasure to the treasureChest
