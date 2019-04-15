716a3b1aeb7626095797c514bcf1de2e51663cef
Winnie Fong <winniefong21@gmail.com>
2019-04-15 10:41:39 -0700

    Implement Bee as subclass of Grub with different age, color and job properties

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,19 @@
-var Bee = function() {
+var Bee = function () {
+  Grub.call(this, Bee);
+  this.age = 5;
+  this.color = 'yellow';
+  this.job = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub.prototype);
+Bee.prototype.constructor = Bee;
+
+// Create a Bee class, in pseudoclassical style, with:
+// call the Grub superclass
+// set the prototype
+// set the constructor
+// an age property that is set to 5
+// a color property that is set to yellow
+// a food property that is inherited from grub
+// an eat method that is inherited from grub
+// a job property that is set to keep on growing
