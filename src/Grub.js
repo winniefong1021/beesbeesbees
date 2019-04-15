e19d9a5fe5cccec87df7f12a55f3f5934d2c7c67
Winnie Fong <winniefong21@gmail.com>
2019-04-15 10:27:08 -0700

    Implement Grub class with age, color, food properties and eat method

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,13 @@
-var Grub = function() {
+var Grub = function () {
+  this.age = 0;
+  this.color = 'pink';
+  this.food = 'jelly';
 };
 
+Grub.prototype.eat = function () { };
+
+// Create a Grub class, in pseudoclassical style, with:
+// an age property that is set to 0
+// a color property that is set to pink
+// a food property that is set to jelly
+// an eat method
